bf8a2e2f1b4ae064ddedd35c213fc0c7d66112b3
rafeen <rafeen1305099@gmail.com>
2020-04-30 01:45:29 +0600

    post method applied

--- Varsity List/all_var.js
+++ Varsity List/all_var.js
@@ -6,7 +6,9 @@ const port = 80;
 const fs = require('fs')
 const filecontent = fs.readFileSync('proj1.html')
 const page2=fs.readFileSync('engi_var.html')
- 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 //mysql
 var mysql = require('mysql');
 
@@ -37,7 +39,7 @@ app.get("/GovtVersity", (req, res)=>{
     
     con.query('select UniversityName from hello.all_var_from_wiki where Type="Public"', function (error, results, fields) {
         if (error) throw error;
-        console.log(results);
+        //console.log(results);
         const params={'va':results,'title':'Public'};
         res.status(200).render('engi_var.pug',params);
     });
@@ -49,7 +51,7 @@ app.get("/PrvVersity", (req, res)=>{
     let str_fi=new Object();
     con.query('select UniversityName from hello.all_var_from_wiki where Type="Private"', function (error, results, fields) {
         if (error) throw error;
-        console.log(results);
+        //console.log(results);
         const params={'va':results,'title':'Private'};
         res.status(200).render('engi_var.pug',params);
     });
@@ -71,7 +73,7 @@ app.get("/AgriculturalVarsity", (req, res)=>{
     let str_fi=new Object();
     con.query('select UniversityName from hello.all_var_from_wiki where Specialization Like "%Agricultural%"', function (error, results, fields) {
         if (error) throw error;
-        console.log(results);
+        //console.log(results);
         const params={'va':results,'title':'Agricultural'};
         res.status(200).render('engi_var.pug',params);
     });
@@ -81,34 +83,49 @@ app.get("/Medical", (req, res)=>{
     let str_fi=new Object();
     con.query('select UniversityName from hello.all_var_from_wiki where Specialization Like "%Medical%"', function (error, results, fields) {
         if (error) throw error;
-        console.log(results);
+        //console.log(results);
         const params={'va':results,'title':'Medical'};
         res.status(200).render('engi_var.pug',params);
     });
 
 });
 app.get("/GeneralVarsity", (req, res)=>{
+    //console.log(req.body)
     let str_fi=new Object();
     con.query('select UniversityName from hello.all_var_from_wiki where Specialization Like "%General%"', function (error, results, fields) {
         if (error) throw error;
-        console.log(results);
+        //console.log(results);
         const params={'va':results,'title':'General'};
         res.status(200).render('engi_var.pug',params);
     });
 
 });
+
 app.get("/Science&TechnologyVarsity", (req, res)=>{
     let str_fi=new Object();
+    console.log(req.body);
     con.query('select UniversityName from hello.all_var_from_wiki where Specialization Like "%Technology%"', function (error, results, fields) {
         if (error) throw error;
-        console.log(results);
+        //console.log(results);
         const params={'va':results,'title':'Science & Technology'};
         res.status(200).render('engi_var.pug',params);
     });
 
 });
+app.post("/More",(req,res)=>{
+    console.log(req.body.name);
+    nick=req.body.name.toString();
+    query_str="select * from hello.all_var_from_wiki where Nickname = '"+nick+"'"
+    con.query(query_str, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results[0])
+        const params={'va':results[0]};
+        res.status(200).render('individul_var.pug',params);
+        
+    });
+});
 
 
-app.listen(port, ()=>{
+app.listen(port,'127.0.0.1', ()=>{
     console.log(`The application started successfully on port ${port}`);
 });
